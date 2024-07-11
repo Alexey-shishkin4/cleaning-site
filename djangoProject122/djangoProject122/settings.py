a71d6ab68b1478cd72eafb6547e14c59eea84171
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2024-07-11 17:52:41 +0700

    edit settings

--- djangoProject122/djangoProject122/settings.py
+++ djangoProject122/djangoProject122/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-be%$_i!az60iq4d8*y(lq^zgq7vik-%z@z@(@xlha21sv1oa!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djazzy.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/djazzy/static'
+
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
